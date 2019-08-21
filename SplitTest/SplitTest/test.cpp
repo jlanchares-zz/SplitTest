2c9c43df47cc5c6095f3bdca8a519e93f409bb13
Jaime Lanchares <jlanchares@hotmail.com>
2019-08-21 12:11:47 +0200

    First test using TDD approach

--- SplitTest/SplitTest/test.cpp
+++ SplitTest/SplitTest/test.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(SplitTestCase, OneSpaceDelimiterTest) {
+	std::vector<std::string> result = SplitTest::split("a b", ' ');
+	EXPECT_EQ(result.size(), 2);
+	EXPECT_EQ(result[0], "a");
+	EXPECT_EQ(result[1], "b");
 }
