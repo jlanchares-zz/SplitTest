225c357bfc01918c7bdf2edaa07957b0beafd6c5
Jaime Lanchares <jlanchares@hotmail.com>
2019-08-21 12:17:38 +0200

    Added static method split for the test to compile

---
+++ SplitTest/SplitTest/SplitTest.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "SplitTest.h"
+
+std::vector<std::string> SplitTest::split(std::string, char delimiter)
+{
+	return std::vector<std::string>();
+}
---
+++ SplitTest/SplitTest/SplitTest.h
@@ -0,0 +1,7 @@
+#pragma once
+class SplitTest
+{
+public:
+	static std::vector<std::string> split(std::string, char delimiter);
+};
+
