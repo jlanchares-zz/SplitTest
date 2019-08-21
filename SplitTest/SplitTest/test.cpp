f5da300fbacf3b8357529b3d5f87428bc9a0fc95
Jaime Lanchares <jlanchares@hotmail.com>
2019-08-21 12:20:56 +0200

    Added multiple tests with some corner cases.

--- SplitTest/SplitTest/test.cpp
+++ SplitTest/SplitTest/test.cpp
@@ -7,3 +7,52 @@ TEST(SplitTestCase, OneSpaceDelimiterTest) {
 	EXPECT_EQ(result[0], "a");
 	EXPECT_EQ(result[1], "b");
 }
+
+TEST(SplitTestCase, TwoSpaceDelimiterTest) {
+	std::vector<std::string> result = SplitTest::split("a b c", ' ');
+	EXPECT_EQ(result.size(), 3);
+	EXPECT_EQ(result[0], "a");
+	EXPECT_EQ(result[1], "b");
+	EXPECT_EQ(result[2], "c");
+}
+
+TEST(SplitTestCase, TwoCommaDelimiterTest) {
+	std::vector<std::string> result = SplitTest::split("a,b,c", ',');
+	EXPECT_EQ(result.size(), 3);
+	EXPECT_EQ(result[0], "a");
+	EXPECT_EQ(result[1], "b");
+	EXPECT_EQ(result[2], "c");
+}
+
+TEST(SplitTestCase, TwoCommaDelimiterNotExpectedTest) {
+	std::vector<std::string> result = SplitTest::split("a b c", ',');
+	EXPECT_EQ(result.size(), 1);
+	EXPECT_EQ(result[0], "a b c");
+}
+
+TEST(SplitTestCase, EmptyStringTest) {
+	std::vector<std::string> result = SplitTest::split("", ',');
+	EXPECT_EQ(result.size(), 0);
+}
+
+TEST(SplitTestCase, OneDelimiterAloneTest) {
+	std::vector<std::string> result = SplitTest::split(",", ',');
+	EXPECT_EQ(result.size(), 0);
+}
+
+TEST(SplitTestCase, MultipleDelimiterAloneTest) {
+	std::vector<std::string> result = SplitTest::split(",,,,,", ',');
+	EXPECT_EQ(result.size(), 0);
+}
+
+TEST(SplitTestCase, CornerCaseLeftDelimiterAloneTest) {
+	std::vector<std::string> result = SplitTest::split("a,", ',');
+	EXPECT_EQ(result.size(), 1);
+	EXPECT_EQ(result[0], "a");
+}
+
+TEST(SplitTestCase, CornerCaseRighttDelimiterAloneTest) {
+	std::vector<std::string> result = SplitTest::split(",a", ',');
+	EXPECT_EQ(result.size(), 1);
+	EXPECT_EQ(result[0], "a");
+}
